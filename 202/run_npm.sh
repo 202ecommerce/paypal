e194ae1b5bf1ed802a11d6a0e8ae92e563598160
bogdan202 <byampolskiy@202-ecommerce.com>
2023-04-21 18:32:22 +0200

    refs #39623 build.xml

--- 202/run_npm.sh
+++ 202/run_npm.sh
@@ -3,3 +3,5 @@
 export NVM_DIR="$HOME/.nvm" # set local path to NVM
 . ~/.nvm/nvm.sh             # add NVM into the Shell session
 nvm use lts/*  # choose current version
+npm install
+npm run build
