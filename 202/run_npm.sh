247b026d59d6d57e42c5341061f5d5ac12f0e7f4
kgleizes <kgleizes@202-ecommerce.com>
2023-04-25 10:42:06 +0200

    refs #39623 - fix Node version to 14.17.3

--- 202/run_npm.sh
+++ 202/run_npm.sh
@@ -2,6 +2,7 @@
 
 export NVM_DIR="$HOME/.nvm" # set local path to NVM
 . ~/.nvm/nvm.sh             # add NVM into the Shell session
-nvm use lts/*  # choose current version
+nvm install 14.17.3
+nvm use 14.17.3  # choose current version
 npm install
 npm run build
