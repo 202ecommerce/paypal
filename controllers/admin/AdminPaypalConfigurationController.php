74a189f8838cbbfbd1b3be799663b21ac9811866
kgleizes <kgleizes@202-ecommerce.com>
2023-08-04 13:45:41 +0200

    refs #41631 - Handle error in a  template

--- controllers/admin/AdminPaypalConfigurationController.php
+++ controllers/admin/AdminPaypalConfigurationController.php
@@ -165,18 +165,18 @@ class AdminPaypalConfigurationController extends \ModuleAdminController
         $result = $paypalOnboarding->execute();
 
         $locale = \Context::getContext()->language->locale;
-        $errorMessage = $this->module->l(
-            'An error occured while trying to link your PayPal\'s account',
-            'AdminPaypalConfigurationController',
-            $locale
-        );
-        $errorMessage .= '<br />' . $this->module->l('More details: ', 'AdminPaypalConfigurationController', $locale) . '<br />';
+        $errorMessages = [
+            $this->module->l(
+                'An error occured while trying to link your PayPal\'s account',
+                'AdminPaypalConfigurationController',
+                $locale
+            ),
+        ];
+        $errorMessages[] = $this->module->l('More details: ', 'AdminPaypalConfigurationController', $locale);
+
         if ($result->isSuccess() == false) {
-            $response->setData([
-                'success' => false,
-                'message' => $errorMessage . $result->getError()->getMessage(),
-            ])->send();
-            exit;
+            $errorMessages[] = $result->getError()->getMessage();
+            $this->errorTemplate($response, $errorMessages);
         }
 
         $authToken = $result->getAuthToken();
@@ -186,11 +186,8 @@ class AdminPaypalConfigurationController extends \ModuleAdminController
         $result = $paypalGetCredentials->execute();
 
         if ($result->isSuccess() == false) {
-            $response->setData([
-                'success' => false,
-                'message' => $result->getError()->getMessage(),
-            ])->send();
-            exit;
+            $errorMessage[] = $result->getError()->getMessage();
+            $this->errorTemplate($response, $errorMessages);
         }
 
         $params = [
@@ -207,6 +204,26 @@ class AdminPaypalConfigurationController extends \ModuleAdminController
             'secret' => $result->getSecret(),
             'merchantId' => $result->getMerchantId(),
             'isSandbox' => $isSandbox,
+        ])->send();
+        exit;
+    }
+
+    /**
+     * Send Error message
+     *
+     * @param JsonResponse $response
+     * @param mixed $messages
+     */
+    private function errorTemplate($response, $messages)
+    {
+        $template = $this->context->smarty->createTemplate(
+            $this->getTemplatePath() . '_partials/alert_ajax.tpl'
+        );
+        $template->assign('messages', $messages);
+
+        $response->setData([
+            'success' => false,
+            'message' => $template->fetch(),
         ])->send();
         exit;
     }
