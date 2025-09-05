a2402f5f9e7336eac1d06186c11308cb15093c92
bogdan202 <byampolskiy@202-ecommerce.com>
2025-09-05 16:56:53 +0200

    phpcsfixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -6,6 +6,7 @@ $config
     ->setUsingCache(true)
     ->getFinder()
     ->in(__DIR__)
+    ->notPath('classes/API/Model/WebhookEvent.php')
     ->exclude(['vendor', 'node_modules', '202']);
 
 return $config;
