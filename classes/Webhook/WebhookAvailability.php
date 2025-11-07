0d8255fc7efd9b8af40a31f89a123593b766d3f9
Clotaire <52157233+clotairer@users.noreply.github.com>
2025-11-07 10:55:11 +0100

    Update SSL version for cURL in WebhookAvailability

--- classes/Webhook/WebhookAvailability.php
+++ classes/Webhook/WebhookAvailability.php
@@ -46,7 +46,7 @@ class WebhookAvailability
         $response = new Response();
         $curl = curl_init((new WebhookHandlerUrl())->getCheckAvailabilityUrl());
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_SSLVERSION, 6);
+        curl_setopt($curl, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
         curl_exec($curl);
         $info = curl_getinfo($curl);
 
