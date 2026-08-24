b991dfd61802e1ee2bd641a48586cc45f5c6c17e
kgleizes <kgleizes@202-ecommerce.com>
2026-08-24 12:55:19 +0200

    refs #57753 - Fix erasing log days Take by default 90 days if not specified and fix return five by default

--- vendor/ppbtlib/src/Extensions/ProcessLogger/ProcessLoggerHandler.php
+++ vendor/ppbtlib/src/Extensions/ProcessLogger/ProcessLoggerHandler.php
@@ -210,7 +210,7 @@ class ProcessLoggerHandler
             return Db::getInstance()->delete(
                 'paypal_processlogger',
                 sprintf(
-                    'date_add <= NOW() - INTERVAL %d DAY',
+                    'date_add <= NOW() - INTERVAL %d DAY AND id_order = 0',
                     self::getAutoErasingDelayInDays()
                 )
             );
@@ -232,13 +232,9 @@ class ProcessLoggerHandler
      */
     public static function getAutoErasingDelayInDays()
     {
-        $numberOfDays = Configuration::get('PAYPAL_EXTLOGS_ERASING_DAYSMAX');
+        $numberOfDays = (int) Configuration::get('PAYPAL_EXTLOGS_ERASING_DAYSMAX', null, null, null, 90);
 
-        if (empty($numberOfDays) || false === is_numeric($numberOfDays)) {
-            return 5;
-        }
-
-        return (int)$numberOfDays;
+        return $numberOfDays;
     }
 
     /**
